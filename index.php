7c713f02d4c96182815e913c294fe372a997a751
AnjaliVed <144322185+AnjaliVed@users.noreply.github.com>
2024-08-03 17:47:54 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,65 @@
+<?php
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Home Page</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+
+<body style="background-color:#fafafa">
+<div class="container">
+<div class="jumbotron" style="background-color:#252626">
+<div class="container text-left" style="background-color:#fafafa; padding-top:30px">
+<img src="image/logo.jpg" class="img-circle" width="200" height="170" align="left"/>
+<h1><b><font color="#dba800">ROYAL RAM JEWELLERS</font></b></h1>
+<div class="col-md-12 text-right text-primary"><font color="#dba800" face="Algerian" size="5"><marquee direction="left" behavior="round">
+<?php
+ 
+echo "Welcome"
+
+ ?></marquee></font>
+</div>
+</div>
+</div>
+</div>
+<?php
+include('menu.php');
+?>
+<img src="image/jiw.jpg" width="700" height="250" style="margin-left: 35px;"></img>
+<p><h4 style= "margin-left: 763px;margin-top: -244px;font-size: 42px;" /><font color="#000" face="Algerian" />"WEARING BEAUTIFUL JEWELLERY MAKES EVERY MOMENT GREAT.."</p>
+<?php
+
+include('try.php');
+?>
+</font></h4>
+<footer class="bg-light text-center text-lg-start">
+<div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+<img src="image/logo.jpg" class="img-circle" width="100" height="100" style="margin-left: -950px;margin-top: 12px;" ></img>
+<h5 style="margin-top: -99px;margin-right: -582px;;"><b><u>ADDRESS</u></b></h5>
+<p style="margin-left: 648px;">Vegetable Market, Purana Gaddi Adda,Kanyashala's backward, Pipliya Mandi</p>
+<p style="margin-left: 648px;">Dist:- Mandsaur, Madhya Pradesh</p>
+<h5 style="margin-top: -115px;margin-left: -337px;"><b><u>CONTACT US</u></b></h5>
+<p style="margin-left: -296px"><b>Email:-</b>RRJEWELLERS_Official@gmail.com</p>
+<p style="margin-left: -296px"><b>Contact No.:-</b>+91 6261200042</p>
+<p style="margin-left: -296px"><b>Phone No.:-</b>28977456</p>
+
+
+  <!-- Copyright -->
+<div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);" style="margin-top: 72px;">
+© 2020 Copyright:
+<a class="text-dark" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+</div>
+  <!-- Copyright -->
+</footer>
+</body>
+</html>
